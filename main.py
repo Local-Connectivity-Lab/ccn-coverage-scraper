28c0c982e3a20c676528375ce4c3e851888cad5b
Paul Philion <pphlilion@istreamplanet.com>
2024-08-23 12:30:46 -0700

    refactoring to depend on chromium driver for CSS selection and page wait handling

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 """
 ccn-coverage-scraper
 """
-#import traceback
-from fastapi import FastAPI, HTTPException, status
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-#from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from fastapi.middleware.cors import CORSMiddleware
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-import time
 import logging
-#import urllib.parse
-import sys
 
-#handler = logging.StreamHandler(sys.stdout)
-#handler.setLevel(logging.DEBUG)
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#handler.setFormatter(formatter)
+import requests
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-logger = logging.getLogger('uvicorn.error')
+logger = logging.getLogger('uvicorn')
 logger.setLevel(logging.DEBUG)
-
-#root = logging.getLogger()
-#root.setLevel(logging.DEBUG)
-#root.addHandler(handler)
-#logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -43,114 +26,66 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 @app.get("/plans")
 def scrape_website(zip_5: str, state: str="", cityname: str="", primary: str="", street_number: str="", st: str="", post_direction: str=""):
     # these variables can be made from previous variable being mashed together
     #state_cap = str.upper(state)
     #cityname_cap = str.capitalize(cityname)
     #street_name = f'{primary}%20{street_number}%20{st}%20{post_direction}'
-    #zip_95 = f'{zip_5}-{zip_9}'
-    #"https://www.allconnect.com/local/{state}/{cityname}?city={cityname_cap}&primary={primary}&street_line={street_name}&street={street_number}%20{st}&postDirection={post_direction}&state={state_cap}&zip9={zip_95}&zip5={zip_5}&zip9or5={zip_95}&prettyAddress={primary}%20{street_number}%20{st}%20{post_direction}%2C%20{cityname_cap}%2C%20{state_cap}%20{zip_95}&zip={zip_95}"
+    #zip_95 = zip_5
+    #url = f"https://www.allconnect.com/local/{state}/{cityname}?city={cityname_cap}&primary={primary}&street_line={street_name}&street={street_number}%20{st}&postDirection={post_direction}&state={state_cap}&zip9={zip_95}&zip5={zip_5}&zip9or5={zip_95}&prettyAddress={primary}%20{street_number}%20{st}%20{post_direction}%2C%20{cityname_cap}%2C%20{state_cap}%20{zip_95}&zip={zip_95}"
 
     allconnect_url = f'https://www.allconnect.com/results/providers?zip={zip_5}'
     logger.info(f"invoking url: {allconnect_url}")
     try:
-        # wifi_providers = get_plans(allconnect_url, 'mb-16 md:mb-24 last:mb-0')
-        wifi_providers = get_plans(allconnect_url, 'mb-16 md:mb-24 last:mb-0')
-
-        logger.info(f"providers found: {wifi_providers}")
+        wifi_providers = get_plans(allconnect_url)
         return wifi_providers
     except:
         logger.exception(f"error logging {allconnect_url}")
 
 
-def get_plans(allconnect_url: str, li_elementclass: str ) -> dict:
+def get_plans(url: str) -> dict:
     # now everything gets packaged together
     wifi_providers = {}
     driver = None
     try:
-        #chrome_options = Options()
-        #chrome_options.add_argument('--headless')
-        chromeOptions = webdriver.ChromeOptions()
-        chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-        chromeOptions.add_argument("--no-sandbox")
-        chromeOptions.add_argument("--disable-setuid-sandbox")
-        chromeOptions.add_argument("--disable-dev-shm-using")
-        chromeOptions.add_argument("--disable-extensions")
-        chromeOptions.add_argument("--disable-gpu")
-        chromeOptions.add_argument("start-maximized")
-        chromeOptions.add_argument("disable-infobars")
-        chromeOptions.add_argument("--headless")
-        #chromeOptions.add_argument(r"user-data-dir=.\cookies\\")
-        #b = webdriver.Chrome(chrome_options=chromeOptions)
-        driver = webdriver.Chrome(options=chromeOptions)
-        #driver.implicitly_wait(5) # 5 seconds wait for page load
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(5) # 5 second timeout
 
-        #this is all of the parameters that are being used to test the url
-        '''
-        state = 'va'
-        cityname= 'arlington'
-        primary = '3109'
-        street_number = '9th'
-        st = "St"
-        post_direction = 'N'
-        zip_5 = '22201'
-        '''
-        #this is the url you type into chrome: http://127.0.0.1:8000/?url=https://www.allconnect.com&state=va&cityname=arlington&primary=3109&street_number=9th&st=St&post_direction=N&zip_5=22201&zip_9=2024
+        driver.get(url)
 
-        #selenium starts up and gets driver page source
-        driver.get(allconnect_url)
-        #li_elements = driver.find_elements(By.CLASS_NAME, "mb-16")
-        time.sleep(5)
-        '''
-        try:
-            # Wait until the element with class "mb-16" appears
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'article.title-card')))
-            logger.debug("Element found!")
-        except TimeoutException:
-            logger.debug("Timed out waiting for element")
-        '''
-        html_text = driver.page_source
-        # beautiful soup finds the relevant data
-        soup = BeautifulSoup(html_text, 'lxml')
-        li_elements = soup.find_all('li', class_=li_elementclass)
-        logger.debug(f'li_elements: {li_elements}')
-        if len(li_elements) == 0:
-            logger.debug(f"no providers found for {allconnect_url}")
-            return {}
-        scrap_data = '\n'.join([li.text for li in li_elements])
-        logger.debug('scrap_data', scrap_data)
+        li_elements = driver.find_elements(By.CSS_SELECTOR, r"li.mb-16.md\:mb-24.last\:mb-0")
 
         # Extracting information for each provider and adding it to the dictionary
-        for li in li_elements:
-            provider_name = li.find(class_='text-center text-gray-steel text-12 leading-10 md:text-left md:text-10 mb-8 md:mt-8') #text-10 leading-10 md:ml-16 lg:ml-0 lg:mb-8
+        for elem in li_elements:
+            provider_name = elem.find_element(By.CSS_SELECTOR, "p.text-12")
+
             if provider_name:
                 provider_name = provider_name.text
-                logger.debug(provider_name)
                 provider_details = {}
 
-                provider_speed = li.find(
-                    class_='flex justify-between items-center text-gray-steel text-14 leading-20 border-solid border-0 border-b border-t border-gray-heather-light md:border-0 py-6 md:py-0 first:border-0 first:py-0 last:border-0 last:py-0 mb-4 lg:w-244').text #product__info-box relative border-b border-solid border-gray-bg-dark px-16 py-10 md:p-16 md:flex md:justify-between md:pr-0 md:pl-16 md:py-0 lg:pl-24 lg:py-24 items-start md:w-1/2 lg:w-1/4
-                speedQ, speedV = 'Available speeds', provider_speed[12:]
-                provider_details[speedQ] = speedV
+                provider_speed = elem.find_element(By.CSS_SELECTOR, r"div.details__container")
+                details = [s.strip() for s in provider_speed.text.split('\n')]
+                provider_details['Available speeds'] = details[1]
+                provider_details['Contract length'] = details[3]
+                provider_details['Connection type'] = details[5]
 
-                provider_price = li.find(class_='flex items-start').text #text-gray-steel text-24 md:text-18 lg:text-28 leading-28 m-0
-                priceQ, priceV = 'Starting at', provider_price
+                provider_price = elem.find_element(By.CSS_SELECTOR, r"div.flex.items-start")
+                prices = [s.strip() for s in provider_price.text.split('\n')]
                 # processing price string to format
-                priceV = priceV.split("/")[0]
-                if len(priceV) == 3:
-                    priceV += ".00"
-                elif len(priceV) == 5:
-                    priceV = priceV[0:3] + "." + priceV[3:]
-                else :
-                    logger.debug(priceV, "don't know price")
-                priceV += "/mo"
-                provider_details[priceQ] = priceV
+                if prices[1].startswith("/"):
+                    # no cents, add ".00"
+                    priceV = prices[0] + ".00" + prices[1]
+                else:
+                    priceV = prices[0] + "." + prices[1]
+                provider_details["Starting at"] = priceV
 
                 wifi_providers[provider_name] = provider_details
-            # Check if wifi_providers is empty
-
-        # Printing the dictionary
+            else:
+                print(f"no provider found for {elem.get_attribute('innerHTML')}")
     except requests.RequestException as e:
         logger.exception("request error")
         raise HTTPException(status_code=500, detail="Failed to fetch URL") from e
--- test.py
+++ test.py
@@ -1,54 +1,68 @@
-#A test document for sorting of webscraped info
+#!/usr/bin/env python3
+# A test document for sorting of webscraped info
 
-from fastapi import FastAPI, HTTPException
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
+import json
+import logging
 import time
 import urllib.parse
-import lxml
-import json
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(options=chrome_options)
+import lxml
+import requests
+from bs4 import BeautifulSoup
+from fastapi import FastAPI, HTTPException
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(5) # 5 seconds wait
 
 test_url = "https://www.allconnect.com/local/va/mechanicsville?city=Mechanicsville&primary=7065&street_line=7065%20Ann%20Cabell%20Ct&street=Ann%20Cabell%20Ct&point=%7B%22latitude%22%3A37.592567%2C%22longitude%22%3A-77.355141%7D&state=VA&streetLine=7065%20Ann%20Cabell%20Ct&zip5=23111&zip9or5=23111&prettyAddress=7065%20Ann%20Cabell%20Ct%2C%20Mechanicsville%2C%20VA%2023111&zip=23111-5227"
 driver.get(test_url)
-time.sleep(5)
 
-html_text = driver.page_source
-
-soup = BeautifulSoup(html_text, 'lxml')
-li_elements = soup.find_all('li', class_='mb-16 last:mb-0')
-scrap_data = '\n'.join([li.text for li in li_elements])
+li_elements = driver.find_elements(By.CSS_SELECTOR, r"li.mb-16.md\:mb-24.last\:mb-0")
 
 wifi_providers = {}
 
 # Extracting information for each provider and adding it to the dictionary
-for li in li_elements:
-    provider_name = li.find('span', class_='text-10 leading-10 md:ml-16 lg:ml-0 lg:mb-8').text
-    print(provider_name)
-    provider_details = {}
+for elem in li_elements:
+    provider_name = elem.find_element(By.CSS_SELECTOR, "p.text-12")
 
-    provider_speed = li.find(class_='product__info-box relative border-b border-solid border-gray-bg-dark px-16 py-10 md:p-16 md:flex md:justify-between md:pr-0 md:pl-16 md:py-0 lg:pl-24 lg:py-24 items-start md:w-1/2 lg:w-1/4').text
-    speedQ, speedV = 'Available speeds',provider_speed[16:]
-    provider_details[speedQ] = speedV
+    if provider_name:
+        provider_name = provider_name.text
+        provider_details = {}
 
-    provider_price = li.find(class_='text-gray-steel text-24 md:text-18 lg:text-28 leading-28 m-0').text
-    priceQ, priceV = 'Starting at', provider_price
-    provider_details[priceQ] = priceV
+        provider_speed = elem.find_element(By.CSS_SELECTOR, r"div.details__container")
+        details = [s.strip() for s in provider_speed.text.split('\n')]
+        provider_details['Available speeds'] = details[1]
+        provider_details['Contract length'] = details[3]
+        provider_details['Connection type'] = details[5]
 
-    wifi_providers[provider_name] = provider_details
+        provider_price = elem.find_element(By.CSS_SELECTOR, r"div.flex.items-start")
+        prices = [s.strip() for s in provider_price.text.split('\n')]
+        # processing price string to format
+        if prices[1].startswith("/"):
+            # no cents, add ".00"
+            priceV = prices[0] + ".00" + prices[1]
+        else:
+            priceV = prices[0] + "." + prices[1]
+        provider_details["Starting at"] = priceV
+
+        wifi_providers[provider_name] = provider_details
+    else:
+        print(f"no provider found for {elem.get_attribute('innerHTML')}")
 
 # Printing the dictionary
 print(wifi_providers)
 
-filename = "data.json"
-with open(filename, 'w') as json_file:
-    json.dump(wifi_providers, json_file, indent=4)
+#filename = "data.json"
+#with open(filename, 'w') as json_file:
+#    json.dump(wifi_providers, json_file, indent=4)
